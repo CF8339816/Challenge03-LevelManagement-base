ae68fb7a9fa8393bea4fa2cd29b16435d73bec7b
CF8339816 <chris_french_3dga@hotmail.com>
2026-01-22 14:26:36 -0400

    working on spawns and the level 2 triggers

--- Assets/Manager/LevelManager.cs
+++ Assets/Manager/LevelManager.cs
@@ -8,24 +8,62 @@ public class LevelManager : MonoBehaviour
 
     public GameObject level01;
     public GameObject level02;
-
-
-
+    public GameObject level03;
+    public GameObject level04;
+    public Gizmo_TriggerSceneChange level2ChangeTriggerN;
+    public Gizmo_TriggerSceneChange level2ChangeTriggerE;
+    public Gizmo_TriggerSceneChange level2ChangeTriggerW;
     private GameObject currentActiveLevel;
-    
-    
- 
+   // public Transform spawnLocation
 
-public void levelChange()
-{
-   
-        
-        
-        
-        
-    level01.SetActive(false);
-    level02.SetActive(true);
+    public void Start()
+    {
+        currentActiveLevel = level01;
 
+    }
+
+    public void levelChange()
+    {
+
+        if (level01)
+        {
+            level01.SetActive(false);
+            level02.SetActive(true);
+          //  PlayerController transform.position = spawnLocation.position;
+        }
+        if (level02)
+        {
+            if (level2ChangeTriggerN)
+            {
+                level02.SetActive(false);
+                level01.SetActive(true);
+            }
+            if (level2ChangeTriggerW)
+            {  level02.SetActive(false);
+            level03.SetActive(true);
+            }
+            if (level2ChangeTriggerE)
+            {
+                level02.SetActive(false);
+                level04.SetActive(true);
+            }
+        }
+
+     if(level03)
+        {
+            level03.SetActive(false);
+            level02.SetActive(true);
+
+        }
+     if (level04)
+        {
+            level04.SetActive(false);
+            level02.SetActive(true);
+
+        }
+
+
+       
 
 
 
