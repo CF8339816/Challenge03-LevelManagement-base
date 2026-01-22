9a51b32c23083f5236eb36ec12d60e2930f157f7
CF8339816 <chris_french_3dga@hotmail.com>
2026-01-22 15:09:47 -0400

    updating code

--- Assets/Manager/LevelManager.cs
+++ Assets/Manager/LevelManager.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
@@ -10,96 +11,79 @@ public class LevelManager : MonoBehaviour
     public GameObject level02;
     public GameObject level03;
     public GameObject level04;
-    public Gizmo_TriggerSceneChange level2ChangeTriggerN;
-    public Gizmo_TriggerSceneChange level2ChangeTriggerE;
-    public Gizmo_TriggerSceneChange level2ChangeTriggerW;
+    //public Gizmo_TriggerSceneChange level2ChangeTriggerN;
+    //public Gizmo_TriggerSceneChange level2ChangeTriggerE;
+    //public Gizmo_TriggerSceneChange level2ChangeTriggerW;
+
+    public GameObject level2ChangeTriggerN;
+    public GameObject level2ChangeTriggerE;
+    public GameObject level2ChangeTriggerW;
+    private GameObject player; 
     private GameObject currentActiveLevel;
-   // public Transform spawnLocation
+    public Transform spawnLocation;
 
     public void Start()
     {
         currentActiveLevel = level01;
-
+        player = ServiceHub.Instance.playerController.gameObject;
     }
 
-    public void levelChange()
+    public void levelChange(GameObject levelToLoad, Transform spawnPoint)
     {
+        currentActiveLevel.SetActive(false);
+        levelToLoad.SetActive(true);
+        currentActiveLevel = levelToLoad;
 
-        if (level01)
-        {
-            level01.SetActive(false);
-            level02.SetActive(true);
-          //  PlayerController transform.position = spawnLocation.position;
-        }
-        if (level02)
-        {
-            if (level2ChangeTriggerN)
-            {
-                level02.SetActive(false);
-                level01.SetActive(true);
-            }
-            if (level2ChangeTriggerW)
-            {  level02.SetActive(false);
-            level03.SetActive(true);
-            }
-            if (level2ChangeTriggerE)
-            {
-                level02.SetActive(false);
-                level04.SetActive(true);
-            }
-        }
-
-     if(level03)
-        {
-            level03.SetActive(false);
-            level02.SetActive(true);
-
-        }
-     if (level04)
-        {
-            level04.SetActive(false);
-            level02.SetActive(true);
-
-        }
+        player.transform.position = spawnPoint.position;
+    }
 
 
-       
+    //public void levelChange()
+    //{
+
+    //    if (level01)
+    //    {
+    //        level01.SetActive(false);
+    //        level02.SetActive(true);
+    //      //  PlayerController transform.position = spawnLocation.position;
+    //    }
+    //    if (level02)
+    //    {
+    //        if (level2ChangeTriggerN)
+    //        {
+    //            level02.SetActive(false);
+    //            level01.SetActive(true);
+    //        }
+    //        if (level2ChangeTriggerW)
+    //        {  
+    //            level02.SetActive(false);
+    //        level03.SetActive(true);
+    //        }
+    //        if (level2ChangeTriggerE)
+    //        {
+    //            level02.SetActive(false);
+    //            level04.SetActive(true);
+    //        }
+    //    }
+
+    //if(level03)
+    //   {
+    //       level03.SetActive(false);
+    //       level02.SetActive(true);
+
+    //   }
+    //if (level04)
+    //   {
+    //       level04.SetActive(false);
+    //       level02.SetActive(true);
+
+    //   }
+
+
+
 
 
 
 
 }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //// Start is called once before the first execution of Update after the MonoBehaviour is created
-    //void Start()
-    //{
-        
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
-}
